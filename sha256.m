31842d8c91f66751f9d68c090ae607a9185170e5
Dominik Januszewicz <Dominik.Januszewicz.PL@ieee.org>
2017-05-13 20:24:27 +0200

    Tablica stałych rund i wartości początkowe

--- sha256.m
+++ sha256.m
@@ -7,6 +7,36 @@ fclose(fid);
 data = de2bi(data,8);
 data = reshape(data,1,size(data,1)*size(data,2));
 
+%The initial hash value H is the following sequence of 32-bit words (which are
+%obtained by taking the fractional parts of the square roots of the first eight primes):
+H = [
+    hex2dec('6a09e667');hex2dec('bb67ae85');hex2dec('3c6ef372');
+    hex2dec('a54ff53a');hex2dec('510e527f');hex2dec('9b05688c');
+    hex2dec('1f83d9ab');hex2dec('5be0cd19')
+    ];
+
+%A sequence of constant rounds K
+K = [
+        '428a2f98'; '71374491'; 'b5c0fbcf'; 'e9b5dba5'; 
+        '3956c25b'; '59f111f1'; '923f82a4'; 'ab1c5ed5';
+        'd807aa98'; '12835b01'; '243185be'; '550c7dc3'; 
+        '72be5d74'; '80deb1fe'; '9bdc06a7'; 'c19bf174';
+        'e49b69c1'; 'efbe4786'; '0fc19dc6'; '240ca1cc';
+        '2de92c6f'; '4a7484aa'; '5cb0a9dc'; '76f988da';
+        '983e5152'; 'a831c66d'; 'b00327c8'; 'bf597fc7';
+        'c6e00bf3'; 'd5a79147'; '06ca6351'; '14292967';
+        '27b70a85'; '2e1b2138'; '4d2c6dfc'; '53380d13'; 
+        '650a7354'; '766a0abb'; '81c2c92e'; '92722c85';
+        'a2bfe8a1'; 'a81a664b'; 'c24b8b70'; 'c76c51a3'; 
+        'd192e819'; 'd6990624'; 'f40e3585'; '106aa070';
+        '19a4c116'; '1e376c08'; '2748774c'; '34b0bcb5'; 
+        '391c0cb3'; '4ed8aa4a'; '5b9cca4f'; '682e6ff3';
+        '748f82ee'; '78a5636f'; '84c87814'; '8cc70208'; 
+        '90befffa'; 'a4506ceb'; 'bef9a3f7'; 'c67178f2'
+    ];
+
+K = hex2dec(K);
+
 %Prepairing step
 
 %Append the bit "1" to the end of the message, and then k zero bits, 
